f8aa17aedfd7fd4e80b0b11ce474b52ba3b430ad
Cotbert <mateojgdev@gmail.com>
2024-10-23 23:45:45 -0500

    open external link

--- src/app/main/comunity-content/comunity-content.component.ts
+++ src/app/main/comunity-content/comunity-content.component.ts
@@ -11,27 +11,31 @@ export class ComunityContentComponent  implements OnInit {
 
   labels : any = [
     {
-      title : 'Important',
+      title : 'Important: ',
       description : 'Lately, instability has been seen on the mars floor, it is reported to use the internal protection of the exploration suits to avoid radiation related illnesses.'
     },
     {
-      title: 'Meeting',
+      title: 'Meeting: ',
       description: 'The leaders of each section are informed about the important meeting about the increase of radiation on the mars floor.'
     },
     {
-      title: 'Entretaiment',
+      title: 'Entretaiment: ',
       description: 'For the start of our exploration mission we invite the crew members to a get-together at the end of the day this week.'
     },
     {
-      title: 'Happy Face',
+      title: 'Happy Face: ',
       description: ' Remember you are an important member of the mission, try to be the best you can, sleep well and work better.'
     },
     {
-      title: 'Work',
+      title: 'Work: ',
       description: 'Remember to complete your work day, or it will be deducted from your final salary plus penalty.'
-    },
+    }
   ]
 
   ngOnInit() {}
+
+  openWebsite() : void {
+    window.open('https://671841c127d55.site123.me/', '_blank');
+  }
 
 }
