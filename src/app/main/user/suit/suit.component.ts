9a8217624629874940fcf5e079fbd61025133a8e
Cotbert <mateojgdev@gmail.com>
2024-10-23 22:58:35 -0500

    accelerometer integration

--- src/app/main/user/suit/suit.component.ts
+++ src/app/main/user/suit/suit.component.ts
@@ -1,17 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  } from '@angular/core';
+import { Motion } from '@capacitor/motion';
 
+//interface for vectors
+interface Vector {
+  x: number;
+  y: number;
+  z: number;
+}
 
 @Component({
   selector: 'app-suit',
   templateUrl: './suit.component.html',
   styleUrls: ['./suit.component.scss'],
 })
+
+
 export class SuitComponent  implements OnInit {
 
+  acceleration : Vector = {x: 0, y: 0, z: 0};
   constructor() { }
 
   ngOnInit() {
 
+    setInterval(() => {
+      this.acceleration.x = this.generateRandomNumber(0, 1);
+      this.acceleration.y = this.generateRandomNumber(0, 1);
+      this.acceleration.z = this.generateRandomNumber(0, 1);
+    }, 100);
+    
+    Motion.addListener('accel', (event) => {
+      this.acceleration.x = event.acceleration.x;
+      this.acceleration.y = event.acceleration.y;
+      this.acceleration.z = event.acceleration.z;
+      console.log('Acceleration: ', this.acceleration);
+    });
+
+  }
+
+   acelerometerPermission()  {
+    alert(this.acceleration.z + ' ' + this.acceleration.y + ' ' + this.acceleration.x);
+    console.log( this.acceleration);
+  }
+
+  //OnDestroy
+  ngOnDestroy() {
+    Motion.removeAllListeners();
+  }
+
+
+  generateRandomNumber(min : number, max : number) : number {
+    return Math.random() * (max - min) + min;
   }
 
 }
--- src/app/main/user/user.component.ts
+++ src/app/main/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Motion } from '@capacitor/motion';
 
 @Component({
   selector: 'app-user',
