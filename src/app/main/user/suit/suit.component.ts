f906d1fc05678ec860f7311e0a56ef544689ffc9
Cotbert <mateojgdev@gmail.com>
2024-10-23 23:25:26 -0500

    temperature and pulse

--- src/app/main/user/suit/suit.component.ts
+++ src/app/main/user/suit/suit.component.ts
@@ -18,14 +18,27 @@ interface Vector {
 export class SuitComponent  implements OnInit {
 
   acceleration : Vector = {x: 0, y: 0, z: 0};
+  temperature : number = 0;
+  pulse1 : string = '12';
+  pulse2 : string = '11';
+
+
+
   constructor() { }
 
   ngOnInit() {
+    this.temperature = this.generateRandomNumber(0, 1);
 
     setInterval(() => {
       this.acceleration.x = this.generateRandomNumber(0, 1);
       this.acceleration.y = this.generateRandomNumber(0, 1);
       this.acceleration.z = this.generateRandomNumber(0, 1);
+    }, 100);
+
+    setInterval(() => {
+      //get just 5 decimal numbers
+      this.pulse1 = `${this.generateRandomNumber(12.00, 12.99)}`.substring(0, 6); 
+      this.pulse2 = `${this.generateRandomNumber(12.00, 12.99)}`.substring(0, 6);
     }, 100);
     
     Motion.addListener('accel', (event) => {
